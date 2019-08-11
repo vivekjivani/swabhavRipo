5882085b5bc15c4bc0381c63ebaf16b6031515d2
vivek jivani <vivekjivani17@gmail.com>
2019-08-11 19:03:05 +0530

    task-management-system :- now database connection is sharable amond sevrel services

--- nodejs/task-management-system/app.js
+++ nodejs/task-management-system/app.js
@@ -2,6 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const UserController = require('./controllers/UserController');
+const mongoose = require('mongoose');
+// const usersSchema = require('../schemas/usersSchema');
+const uri = "mongodb+srv://admin:root@cluster0-sjbot.mongodb.net/"
+    + "task_managemnet_system?retryWrites=true&w=majority";
+
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(uri, { useNewUrlParser: true })
+    .then(
+        (result) => {
+            console.log('database connected..');
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error);
+        }
+    );
 let app = express();
 
 const PORT = 9090;
--- nodejs/task-management-system/services/userService.js
+++ nodejs/task-management-system/services/userService.js
@@ -1,38 +1,7 @@
-const mongoose = require('mongoose');
-// const usersSchema = require('../schemas/usersSchema');
-const uri = 'mongodb+srv://admin:root@cluster0-sjbot.mongodb.net/task_managemnet_system?retryWrites=true&w=majority'
 const userModel = require('../schemas/usersSchema');
-
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
 
 class UserService {
     constructor() {
-        this._connect();
-    }
-
-    _connect() {
-        mongoose.connect(uri, { useNewUrlParser: true })
-            .then(
-                (result) => {
-                    console.log('database connected..');
-                }
-            )
-            .catch(
-                (error) => {
-                    console.log(error);
-                }
-            );
-
-        let connection = mongoose.connection;
-        // connection.on('connected', () => {
-        //     console.log('db connected');
-        // });
-
-        connection.on('error', (error) => {
-            console.log(error);
-        });
     }
 
     getUserId(email) {
